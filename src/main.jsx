fb674e00c6d87096f1588a8f725fd7c32eb96de8
suhailvs <suhailvs@gmail.com>
2026-09-02 05:46:21 +0530

    removed settings and auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@tabler/icons-webfont/dist/tabler-icons.min.css';
-import { AuthProvider } from './utils/AuthContext';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
-    <AuthProvider>
-        <App />
-      </AuthProvider>
+    <App />
   </BrowserRouter>
   </StrictMode>,
 )
